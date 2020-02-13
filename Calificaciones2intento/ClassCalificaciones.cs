0ae9f216129cabf13d45fbb43322e019cff2f39c
JCM-19 <josvaldejesus@gmail.com>
2020-02-13 09:34:27 -0700

    Calificaciones

---
+++ Calificaciones2intento/ClassCalificaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calificaciones20
+{
+    class ClassCalificaciones
+    {
+        //Atributos de la clase 
+        public int cal = 0;
+        public int aprobados = 0;
+        public int reprobados = 0;
+        int contador = 0;
+        public double promedio = 0;
+        int suma = 0;
+        
+        //Métodos para retornar promedio 
+        public void capturarCalificación()     
+        {
+            if (cal >= 70)
+            {
+                aprobados = aprobados + 1;
+                suma = suma + cal;
+            }       
+            else
+            {
+                reprobados = reprobados + 1;
+                suma = suma + cal;
+            }
+            contador++;
+            promedio = suma / contador;   
+        }
+    }
+}
---
+++ Calificaciones2intento/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Calificaciones2intento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.txtCalificación = new System.Windows.Forms.TextBox();
+            this.lblCalificación = new System.Windows.Forms.Label();
+            this.lblAprobados = new System.Windows.Forms.Label();
+            this.lblReprobados = new System.Windows.Forms.Label();
+            this.lbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapturar.Location = new System.Drawing.Point(263, 36);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(75, 23);
+            this.btnCapturar.TabIndex = 0;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReporte.Location = new System.Drawing.Point(263, 94);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(75, 23);
+            this.btnReporte.TabIndex = 1;
+            this.btnReporte.Text = "Reporte";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
+            // 
+            // txtCalificación
+            // 
+            this.txtCalificación.Location = new System.Drawing.Point(150, 38);
+            this.txtCalificación.Name = "txtCalificación";
+            this.txtCalificación.Size = new System.Drawing.Size(100, 20);
+            this.txtCalificación.TabIndex = 2;
+            // 
+            // lblCalificación
+            // 
+            this.lblCalificación.AutoSize = true;
+            this.lblCalificación.Font = new System.Drawing.Font("Century Gothic", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalificación.Location = new System.Drawing.Point(21, 40);
+            this.lblCalificación.Name = "lblCalificación";
+            this.lblCalificación.Size = new System.Drawing.Size(126, 15);
+            this.lblCalificación.TabIndex = 3;
+            this.lblCalificación.Text = "Calificación obtenida:";
+            // 
+            // lblAprobados
+            // 
+            this.lblAprobados.AutoSize = true;
+            this.lblAprobados.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAprobados.Location = new System.Drawing.Point(45, 84);
+            this.lblAprobados.Name = "lblAprobados";
+            this.lblAprobados.Size = new System.Drawing.Size(69, 15);
+            this.lblAprobados.TabIndex = 4;
+            this.lblAprobados.Text = "Aprobados:";
+            // 
+            // lblReprobados
+            // 
+            this.lblReprobados.AutoSize = true;
+            this.lblReprobados.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReprobados.Location = new System.Drawing.Point(39, 113);
+            this.lblReprobados.Name = "lblReprobados";
+            this.lblReprobados.Size = new System.Drawing.Size(75, 15);
+            this.lblReprobados.TabIndex = 5;
+            this.lblReprobados.Text = "Reprobados:";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(133, 104);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(0, 13);
+            this.lbl.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 7;
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.AutoSize = true;
+            this.lblPromedio.Font = new System.Drawing.Font("Century Gothic", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPromedio.Location = new System.Drawing.Point(27, 141);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(87, 15);
+            this.lblPromedio.TabIndex = 8;
+            this.lblPromedio.Text = "Promedio total:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(350, 258);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPromedio);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.lblReprobados);
+            this.Controls.Add(this.lblAprobados);
+            this.Controls.Add(this.lblCalificación);
+            this.Controls.Add(this.txtCalificación);
+            this.Controls.Add(this.btnReporte);
+            this.Controls.Add(this.btnCapturar);
+            this.Name = "Form1";
+            this.Text = "Captura de calificaciones";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.Button btnReporte;
+        private System.Windows.Forms.TextBox txtCalificación;
+        private System.Windows.Forms.Label lblCalificación;
+        private System.Windows.Forms.Label lblAprobados;
+        private System.Windows.Forms.Label lblReprobados;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPromedio;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
