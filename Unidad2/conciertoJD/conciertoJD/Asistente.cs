4494e4f5a940ca7c07e2398b598f0c833616fde7
JCM-19 <josvaldejesus@gmail.com>
2020-02-26 18:50:38 -0700

    Concierto

---
+++ Unidad2/conciertoJD/conciertoJD/Asistente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conciertoJD
+{
+     public class Asistente
+    {
+        //Atributos de la clase
+        public int Bebé = 0, Niño = 0, Adulto = 0, Terceraedad = 0;
+        public int num1 = 0, num2 = 0, num3 = 0, num4 = 0;
+        public string personas;
+        //Metodos de la clase
+        public void Registro ()
+        {
+            switch (personas)
+            {
+                case "Bebé":
+                    Bebé = Bebé + num1;
+                    break;
+                case "Niño":
+                    Niño = Niño + num2;
+                    break;
+                case "Adulto":
+                    Adulto = Adulto + num3;
+                    break;
+                case "Tercera edad":
+                    Terceraedad = Terceraedad + num4;
+                    break;
+            }
+        }
+    }
+}
---
+++ Unidad2/conciertoJD/conciertoJD/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace conciertoJD
+{
+    public partial class Form1 : Form
+    {
+        Asistente objConcierto = new Asistente();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            objConcierto.personas = cmbPersonas.Text.ToString();
+            objConcierto.num1 = int.Parse(cmbNumero.Text.ToString());
+            objConcierto.num2 = int.Parse(cmbNumero.Text.ToString());
+            objConcierto.num3=int.Parse(cmbNumero.Text.ToString());
+            objConcierto.num4 = int.Parse(cmbNumero.Text.ToString());
+            objConcierto.Registro();
+            cmbNumero.Text = "";
+            cmbPersonas.Text = "";
+            MessageBox.Show("¡Listo!");
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label7.Text = objConcierto.Bebé.ToString();
+            label8.Text = objConcierto.Niño.ToString();
+            label9.Text = objConcierto.Adulto.ToString();
+            label10.Text = objConcierto.Terceraedad.ToString();
+        }
+    }
+}
